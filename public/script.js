85b808562d03c9c209aca6339a63cc80477b9116
Advancial <dillomat002@gmail.com>
2026-08-04 19:03:30 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -151,10 +151,20 @@ let localStream = null;
 let peer = null;
 let pendingIceCandidates = [];
 
+// Reliable STUN + TURN Relay configuration
 const rtcConfig = {
     iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:stun1.l.google.com:19302" }
+        { urls: "stun:stun1.l.google.com:19302" },
+        {
+            urls: [
+                "turn:openrelay.metered.ca:80",
+                "turn:openrelay.metered.ca:443",
+                "turn:openrelay.metered.ca:443?transport=tcp"
+            ],
+            username: "openrelayproject",
+            credential: "openrelayproject"
+        }
     ]
 };
 
